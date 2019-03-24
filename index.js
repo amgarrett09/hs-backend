c9d1b1409f9cad443632d914c9e68eea03594b0f
Alex Garrett <agarrettR8@gmail.com>
2019-03-24 00:36:52 -0400

    added allowed origins

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const mongoose = require("mongoose");
 const config = require("./config");
 const corsMiddleware = require("restify-cors-middleware");
 const AutoComplete = require("./lib/autocomplete/index");
-const Player = require('./models/Player');
+const Player = require("./models/Player");
 
 const cors = corsMiddleware({
   preflightMaxAge: 5,
-  origins: ["http://localhost:3000", "https://hockey-scrub.herokuapp.com"],
+  origins: [
+    "http://localhost:3000",
+    "https://hockey-scrub.site",
+    "https://www.hockey-scrub.site"
+  ],
   allowHeaders: ["API-token"],
   exposeHeaders: ["API-Token-Expiry"]
 });
